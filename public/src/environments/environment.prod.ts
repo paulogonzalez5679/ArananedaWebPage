177aece0eeccb2b2bf5852687cb9c8418976b7d7
Paulogonzalez5679 <paulomga10@gmail.com>
2022-06-06 14:23:54 -0500

    version final de araneda

---
+++ public/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBf0omTQafoTEFF2sXb3L1OjTPXA7ooXnM",
+    authDomain: "aranedaweb-aa584.firebaseapp.com",
+    databaseURL: "https://aranedaweb-aa584.firebaseio.com",
+    projectId: "aranedaweb-aa584",
+    storageBucket: "aranedaweb-aa584.appspot.com",
+    messagingSenderId: "602629657686",
+    appId: "1:602629657686:web:0c6b1a2197a2af4bc7d7f7"
+  },
+};
---
+++ public/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBf0omTQafoTEFF2sXb3L1OjTPXA7ooXnM",
+    authDomain: "aranedaweb-aa584.firebaseapp.com",
+    databaseURL: "https://aranedaweb-aa584.firebaseio.com",
+    projectId: "aranedaweb-aa584",
+    storageBucket: "aranedaweb-aa584.appspot.com",
+    messagingSenderId: "602629657686",
+    appId: "1:602629657686:web:0c6b1a2197a2af4bc7d7f7"
+
+    
+  },
+};
